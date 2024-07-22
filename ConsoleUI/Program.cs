c811f637a4cf714209cfed1dd689361e52974a89
Dan Marino <technicaldanny302@gmail.com>
2024-07-21 19:13:49 -0700

    completed exercise

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -17,7 +17,7 @@ namespace ConsoleUI
 
             #region Vehicles
 
-            /*
+            /*Done
              * Create an abstract class called Vehicle
              * The vehicle class shall have three string properties: Year, Make, and Model
              * Set the defaults of the properties to something generic in the Vehicle class
@@ -25,7 +25,7 @@ namespace ConsoleUI
              * Vehicle class shall have a virtual method called DriveVirtual with a base implementation.
              */
 
-            /* 
+            /* Done
              * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
              * Add a distinct property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
              * Provide the implementations for the abstract methods
@@ -33,21 +33,42 @@ namespace ConsoleUI
             */
 
             // Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
 
-            /*
-             * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
-             * 
-             * Set the properties values with object initializer syntax
-             */
+
+            //*Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle(use explicit typing) but use constuctors from derived classes
+            // * Set the properties values with object initializer syntax
+
+            Car focus = new Car() { HasTrunk = true, Make = "Ford", Model = "Focus", Year = 2013 };
+            var Motorcycle = new Motorcycle() { HasSideCart = true, Make = "HD", Model = "Chopper", Year = 1978 };
+
+            Vehicle sedan = new Car() { HasTrunk = true, Make = "Doge", Model = "Blah", Year = 2019 };
+            Vehicle sport = new Car() { HasTrunk = false, Make = "Ferrari", Model = "Something", Year = 2000 };
+
+
+
 
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate through the list and display each of the properties
              */
+ 
+
+            vehicles.Add(focus);
+            vehicles.Add(sedan);
+            vehicles.Add(sport);
+            vehicles.Add(Motorcycle);
+
+            foreach (var veh in vehicles)
+            {
+                Console.WriteLine($"Make {veh.Make} Model {veh.Model} Year {veh.Year}");
+                veh.DriveVirtual();
+                Console.WriteLine("-----------------------");
+            }
 
             // Call each of the drive methods for one car and one motorcycle
 
-            #endregion            
+            #endregion
             Console.ReadLine();
         }
     }
